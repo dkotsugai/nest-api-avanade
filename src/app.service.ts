0ec24511fa27fbdd47da98b3d472f47b2f59acee
Daniel Kotsugai <d.kotsugai.sarmento@avanade.com>
2022-10-24 15:53:14 -0300

    deploy24-10

---
+++ src/app.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AppService {
+  getHello(): string {
+    return 'Hello World!';
+  }
+
+  getLogin(): string {
+    return 'Aqui virá o login';
+  }
+
+  getRegister(): string {
+    return 'Aqui virá o cadastro';
+  }
+}
